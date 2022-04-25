c64a5d359a1d360498f058a8208a07586c8535ef
suu3 <ahob789@ewhain.net>
2022-04-25 19:22:33 +0900

    feat: next.js eslint, prettier, 기타 라이브러리 설치

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Paws</title>
+        <meta name="description" content="Numble Short From Video Challenge" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Paws</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;

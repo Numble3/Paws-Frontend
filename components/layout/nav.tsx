5085ed8d65152dd9d867d4a09f2161d60fde2e70
yena <center7332@naver.com>
2022-05-03 00:25:06 +0900

    feat: navigation layout

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,22 +1,42 @@
+import { ICONS } from "lib/assets";
+import Image from "next/image";
 import Link from "next/link";
+import { memo } from "react";
+import styles from "styles/layout.module.css";
 
-export default function Nav() {
+const Nav = () => {
   return (
-    <ul>
-      <li>
-        <Link href="/">홈</Link>
-      </li>
-      <li>
-        <Link href="/myVideo">마이 비디오</Link>
-      </li>
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <Link href="/">
+            <Image src={ICONS.HOME} width={20} height={20} />
+          </Link>
+        </li>
+        <li>
+          <Link href="/myVideo">
+            <Image src={ICONS.CATEGORY} width={20} height={20} />
+          </Link>
+        </li>
+        <li className={styles.upload}>
+          <Link href="/upload">
+            <Image src={ICONS.UPLOAD} width={20} height={20} />
+          </Link>
+        </li>
+        <li>
+          <Link href="/interestVideo">
+            <Image src={ICONS.LIKE} width={20} height={20} />
+          </Link>
+        </li>
 
-      <li>
-        <Link href="/interestVideo">관심영상</Link>
-      </li>
-
-      <li>
-        <Link href="/profile">프로필</Link>
-      </li>
-    </ul>
+        <li>
+          <Link href="/profile">
+            <Image src={ICONS.PROFILE} width={20} height={20} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
-}
+};
+
+export default memo(Nav);

ce2ba0bea46fbd8a40bc45c98d7d34bca6eab27b
yena <center7332@naver.com>
2022-05-09 23:15:45 +0900

    feat: update navigation with highlight color

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,39 +1,60 @@
+import {
+  CategoryIcon,
+  HomeIcon,
+  LikeIcon,
+  ProfileIcon,
+} from "components/icons";
 import { ICONS } from "lib/assets";
 import Image from "next/image";
 import Link from "next/link";
-import { memo } from "react";
+import { useRouter } from "next/router";
+import { memo, useMemo } from "react";
 import styles from "styles/layout.module.css";
 
 const Nav = () => {
+  const router = useRouter();
+
+  const navItems = useMemo(
+    () => [
+      {
+        path: "/",
+        icon: <HomeIcon isActive={router.asPath === "/"} />,
+      },
+      {
+        path: "/search",
+        icon: <CategoryIcon isActive={router.asPath === "/search"} />,
+      },
+      {
+        path: "/upload",
+        icon: <Image width={20} height={20} src={ICONS.UPLOAD} />,
+        hasCircle: true,
+      },
+      {
+        path: "/interestVideo",
+        icon: <LikeIcon isActive={router.asPath === "/interestVideo"} />,
+      },
+      {
+        path: "/profile",
+        icon: <ProfileIcon isActive={router.asPath === "/profile"} />,
+      },
+    ],
+    [router]
+  );
+
   return (
     <nav className={styles.nav}>
       <ul>
-        <li>
-          <Link href="/">
-            <Image src={ICONS.HOME} width={20} height={20} />
+        {navItems.map((v, i) => (
+          <Link href={v.path} key={i}>
+            <li
+              className={`${v.hasCircle ? styles.upload : ""} ${
+                router.asPath === v.path ? styles.highlight : ""
+              }`}
+            >
+              {v.icon}
+            </li>
           </Link>
-        </li>
-        <li>
-          <Link href="/search">
-            <Image src={ICONS.CATEGORY} width={20} height={20} />
-          </Link>
-        </li>
-        <li className={styles.upload}>
-          <Link href="/upload">
-            <Image src={ICONS.UPLOAD} width={20} height={20} />
-          </Link>
-        </li>
-        <li>
-          <Link href="/interestVideo">
-            <Image src={ICONS.LIKE} width={20} height={20} />
-          </Link>
-        </li>
-
-        <li>
-          <Link href="/profile">
-            <Image src={ICONS.PROFILE} width={20} height={20} />
-          </Link>
-        </li>
+        ))}
       </ul>
     </nav>
   );

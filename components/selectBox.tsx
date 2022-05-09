46311a1391b8a0551ff5a648fcc0e022579198e8
suu3 <ahob789@ewhain.net>
2022-05-09 23:09:54 +0900

    refactor: select box value => object로

--- components/selectBox.tsx
+++ components/selectBox.tsx
@@ -3,38 +3,46 @@ import styles from "styles/selectBox.module.css";
 import Image from "next/image";
 import { ICONS } from "lib/assets";
 
+type SelectType = {
+  value: string;
+  desc: string;
+};
 const SelectBox = () => {
-  const [selectedValue, setSelectedValue] = useState("최신순");
+  const selectOption = [
+    { value: "latest", desc: "최신순" },
+    { value: "popular", desc: "인기순" },
+  ];
+  const [selectedValue, setSelectedValue] = useState(selectOption[0]);
   const [isSelected, setIsSelected] = useState(false);
 
-  const handleSelectBox = (value: string) => {
-    setSelectedValue(value);
+  const handleSelectBox = ({ value, desc }: SelectType) => {
+    setSelectedValue({ value, desc });
   };
   return (
     <div className={styles.select} onClick={() => setIsSelected(!isSelected)}>
       <div className={isSelected ? styles.selected_focused : styles.selected}>
-        <div>{selectedValue}</div>
+        <div>{selectedValue.desc}</div>
         <Image src={ICONS.DOWN_ARROW} width={12} height={12} />
       </div>
       {isSelected && (
         <ul>
           <li
-            onClick={() => handleSelectBox("최신순")}
+            onClick={() => handleSelectBox(selectOption[0])}
             className={styles.option}
           >
             최신순
-            {selectedValue === "최신순" ? (
+            {selectedValue.desc === selectOption[0].desc ? (
               <Image src={ICONS.ACTIVE} width={4} height={4} />
             ) : (
               <></>
             )}
           </li>
           <li
-            onClick={() => handleSelectBox("인기순")}
+            onClick={() => handleSelectBox(selectOption[1])}
             className={styles.option}
           >
             인기순
-            {selectedValue === "인기순" && (
+            {selectedValue.desc === selectOption[1].desc && (
               <Image src={ICONS.ACTIVE} width={4} height={4} />
             )}
           </li>

8bd7843f42adf04e6c88aaa4dd0f3b0d4e49d9f3
suu3 <ahob789@ewhain.net>
2022-05-09 17:10:19 +0900

    design: 임베드 페이지 css

--- pages/upload/embed.tsx
+++ pages/upload/embed.tsx
@@ -27,48 +27,66 @@ const Embed = () => {
   };
 
   return (
-    <>
-      <section className={styles.link}>
+    <section className={styles.wrap}>
+      <div className={styles.link}>
         <p className={styles.title}>영상 임베드 링크</p>
-        <input />
-      </section>
-      <section className={styles.thumbnail}>
+        <input
+          className={styles.text_input}
+          placeholder="링크 주소를 입력해주세요"
+        />
+      </div>
+      <div className={styles.thumbnail}>
         <p className={styles.title}>썸네일 이미지</p>
-        <div className={styles.preview}>
-          {imageSrc ? (
-            <img src={imageSrc} alt="preview-img" />
-          ) : (
-            <div>회색 배경</div>
-          )}
-        </div>
-        <div className={styles.image__icon_row}>
-          <input
-            type="file"
-            name="imageFile"
-            onChange={(e) => {
-              encodeFileToBase64(e.target.files[0] as File);
-            }}
-          />
-          <label htmlFor="imageFile"></label>
-          <div className={styles.trash}></div>
-          <div className={styles.image_loading}>
-            {isImageUpload ? (
-              <div>녹색아이콘</div>
-            ) : (
-              <div>회색 체크 아이콘</div>
-            )}
+        <div>
+          <div className={styles.preview_row}>
+            <div className={styles.preview}>
+              {imageSrc ? (
+                <img src={imageSrc} alt="preview-img" />
+              ) : (
+                <div>
+                  <div>
+                    <Image src={ICONS.GALLERY} width={25} height={25} />
+                  </div>
+                  <span>이미지를 업로드 해주세요!</span>
+                </div>
+              )}
+            </div>
+            <span>최대 용량 : 10mb</span>
+          </div>
+          <div className={styles.image__icon_row}>
+            <input
+              type="file"
+              name="imageFile"
+              onChange={(e) => {
+                encodeFileToBase64(e.target.files[0] as File);
+              }}
+            />
+            <label htmlFor="imageFile"></label>
+            <div className={styles.trash}></div>
+            <div className={styles.image_loading}>
+              {isImageUpload ? (
+                <div>녹색아이콘</div>
+              ) : (
+                <div className={styles.image_success}>
+                  <Image src={ICONS.SUCCESS} width={25} height={25} />
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </section>
-      <section className={styles.video_name}>
+      </div>
+      <div className={styles.video_name}>
         <p className={styles.title}>영상 제목</p>
-        <input />
-      </section>
-      <section className={styles.description}>
+        <input
+          className={styles.text_input}
+          placeholder="영상 제목을 입력해주세요"
+        />
+      </div>
+      <div className={styles.description}>
         <p className={styles.title}>설명</p>
-        <textarea />
-      </section>
-      <section className={styles.category}>
+        <textarea placeholder="설명 내용을 입력해주세요" />
+      </div>
+      <div className={styles.category}>
         <p className={styles.title}>
           카테고리 <span>중복 선택 불가</span>
         </p>
@@ -77,8 +95,8 @@ const Embed = () => {
             <li>{name}</li>
           ))}
         </ul>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 

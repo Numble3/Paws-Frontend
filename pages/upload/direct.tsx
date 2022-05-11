f3ad387d6acb67ed2d350bd8082179ae843f7a1f
suu3 <ahob789@ewhain.net>
2022-05-11 23:21:37 +0900

    fix: input 교체

--- pages/upload/direct.tsx
+++ pages/upload/direct.tsx
@@ -1,19 +1,34 @@
 import styles from "styles/upload/direct.module.css";
-import { ICONS, IMAGES } from "lib/assets";
-import Image from "next/image";
 import PreviewImage from "components/upload/previewImage";
 import PreviewVideo from "components/upload/previewVideo";
 import VideoCategory from "components/upload/videoCategory";
-import VideoTitle from "components/upload/videoTitle";
+import { CustomInput } from "components/custom";
+import { useState } from "react";
 
 const Direct = () => {
+  //title
+  const [titleError, setTitleError] = useState({
+    isError: false,
+    message: "제목을 입력해주세요.",
+  });
+  const [title, setTitle] = useState("");
   return (
     <section className={styles.wrap}>
       <PreviewVideo />
       <div className="border-gray"></div>
       <PreviewImage />
       <div className="border-gray"></div>
-      <VideoTitle />
+      <div className={styles.video_name}>
+        <p className={styles.title}>영상 제목</p>
+        <CustomInput
+          error={titleError}
+          inputType={"text"}
+          placeHolderMessage={"영상 제목을 입력해주세요"}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </div>
       <div className="border-gray"></div>
       <VideoCategory />
     </section>
--- pages/upload/embed.tsx
+++ pages/upload/embed.tsx
@@ -1,46 +1,52 @@
 import styles from "styles/upload/embed.module.css";
 import PreviewImage from "components/upload/previewImage";
 import VideoCategory from "components/upload/videoCategory";
-import VideoTitle from "components/upload/videoTitle";
 import { ChangeEvent, useState, useEffect } from "react";
 import axios from "axios";
 import { CustomInput } from "components/custom";
 
 const Embed = () => {
   const URL = "https://www.youtube.com/oembed?url=";
-  const [canUpload, setCanUpload] = useState({
+  //link
+  const [linkError, setLinkError] = useState({
     isError: false,
     message: "올바르지 않은 링크 주소입니다.",
   });
   const [linkSuccess, setLinkSuccess] = useState(false);
   const [link, setLink] = useState("");
+  //title
+  const [titleError, setTitleError] = useState({
+    isError: false,
+    message: "제목을 입력해주세요.",
+  });
+  const [title, setTitle] = useState("");
   const checkURL = async (e: ChangeEvent<HTMLInputElement>) => {
     setLink(e.target.value);
     try {
       const res = await axios.get(URL + e.target.value);
       if (res.data) {
-        setCanUpload({ ...canUpload, isError: false });
+        setLinkError({ ...linkError, isError: false });
         setLinkSuccess(true);
       }
     } catch (e) {
-      setCanUpload({ ...canUpload, isError: true });
+      setLinkError({ ...linkError, isError: true });
       setLinkSuccess(false);
     }
   };
   useEffect(() => {
     if (link === "") {
-      setCanUpload({ ...canUpload, isError: false });
+      setLinkError({ ...linkError, isError: false });
       setLinkSuccess(false);
     }
   }, [link]);
   return (
     <section className={styles.wrap}>
       <span className={styles.complete}>완료</span>
-      <div className={styles.link}>
+      <div>
         <p className={styles.title}>영상 임베드 링크</p>
         <CustomInput
           isSuccess={linkSuccess}
-          error={canUpload}
+          error={linkError}
           inputType={"text"}
           placeHolderMessage={"링크 주소를 입력해주세요"}
           onChange={checkURL}
@@ -49,7 +55,17 @@ const Embed = () => {
       <div className="border-gray"></div>
       <PreviewImage />
       <div className="border-gray"></div>
-      <VideoTitle />
+      <div className={styles.video_name}>
+        <p className={styles.title}>영상 제목</p>
+        <CustomInput
+          error={titleError}
+          inputType={"text"}
+          placeHolderMessage={"영상 제목을 입력해주세요"}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </div>
       <div className="border-gray"></div>
       <div className={styles.description}>
         <p className={styles.title}>설명</p>
